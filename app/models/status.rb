4f3400dd55911d8b6fed6f7069eb829633d63c10
Deiver <cutiva@live.ca>
2013-10-19 18:22:46 -0400

    Created a rails app and also added status updates :)

---
+++ app/models/status.rb
@@ -0,0 +1,3 @@
+class Status < ActiveRecord::Base
+  attr_accessible :active_record, :content, :invoke, :name
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boombang::Application.config.secret_token = '933fa27e9eb9f0fa9a05b67c0cdc38ceef9f40cf1d63d1eb57f9f28813dd2837abbe32a0c10af554ed6857bb16e8ecd8b3e2697e9471b0e7c60b7dc5bb92361a'
